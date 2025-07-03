1ff66b9225f4c2d7cd958a95332193eded87c94a
rxnjitth <mranjith2506@gmail.com>
2025-07-03 15:07:23 +0530

    settings updated

--- intern_site/settings.py
+++ intern_site/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': os.environ.get('MYSQL_DATABASE'),
         'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'HOST': os.environ.get('MYSQL_HOST'),
         'PORT': os.environ.get('MYSQL_PORT', '3306'),
     }
 }
